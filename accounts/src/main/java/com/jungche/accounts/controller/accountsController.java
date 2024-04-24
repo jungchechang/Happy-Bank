f603621eaf3d00c9ef0873653e9d8403aa8d7ea4
JungCheChang <jungche0408@gmail.com>
2024-04-24 16:31:45 -0700

    Update accountsController

--- accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
+++ accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
@@ -1,15 +1,36 @@
 package com.jungche.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.jungche.accounts.constants.AccountsConstants;
+import com.jungche.accounts.dto.CustomerDto;
+import com.jungche.accounts.dto.ResponseDto;
+import com.jungche.accounts.service.IAccountsService;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class accountsController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "123";
+    private IAccountsService iAccountsService;
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(
+                        AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                           @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                           String mobileNumber) {
+        CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 }
