0e427c6dd62ca69cd2117b9a144fd99c0c0b38fb
JungCheChang <jungche0408@gmail.com>
2024-04-24 19:51:50 -0700

    Add validation

--- accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
+++ accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
@@ -10,16 +10,18 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class accountsController {
 
     private IAccountsService iAccountsService;
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
