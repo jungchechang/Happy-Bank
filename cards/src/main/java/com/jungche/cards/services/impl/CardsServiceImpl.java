c1984ca1a7c77d373e864bad1dcda9bc90f798f6
JungCheChang <jungche0408@gmail.com>
2024-04-25 11:14:45 -0700

    Add cards service

---
+++ cards/src/main/java/com/jungche/cards/services/impl/CardsServiceImpl.java
@@ -0,0 +1,61 @@
+package com.jungche.cards.services.impl;
+
+import com.jungche.cards.constants.CardsConstants;
+import com.jungche.cards.dto.CardsDto;
+import com.jungche.cards.entity.Cards;
+import com.jungche.cards.exception.CardAlreadyExistsException;
+import com.jungche.cards.repository.CardsRepository;
+import com.jungche.cards.services.ICardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class CardsServiceImpl implements ICardsService {
+    private CardsRepository cardsRepository;
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    @Override
+    public void createCard(String mobileNumber) {
+        Optional<Cards> optionalCards= cardsRepository.findByMobileNumber(mobileNumber);
+        if(optionalCards.isPresent()){
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber "+mobileNumber);
+        }
+        cardsRepository.save(createNewCard(mobileNumber));
+    }
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new card details
+     */
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
