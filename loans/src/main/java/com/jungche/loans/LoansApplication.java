bb800193583aa7b51d9b860a92473f17bbee971f
JungCheChang <jungche0408@gmail.com>
2024-04-25 16:22:58 -0700

    Add API documentation

--- loans/src/main/java/com/jungche/loans/LoansApplication.java
+++ loans/src/main/java/com/jungche/loans/LoansApplication.java
@@ -1,9 +1,31 @@
 package com.jungche.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "HappyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Jung-Che",
+						email = "example@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "HappyBank Loans microservice REST API Documentation"
+		)
+)
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
