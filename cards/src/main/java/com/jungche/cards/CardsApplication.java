ceb7ab146eeec4df00689d45908192e2562f09ed
JungCheChang <jungche0408@gmail.com>
2024-04-25 11:34:40 -0700

    Add API documentation

--- cards/src/main/java/com/jungche/cards/CardsApplication.java
+++ cards/src/main/java/com/jungche/cards/CardsApplication.java
@@ -1,9 +1,31 @@
 package com.jungche.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "HappyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Jung-Che Chang",
+						email = "example@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "HappyBank Cards microservice REST API Documentation"
+		)
+)
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class CardsApplication {
