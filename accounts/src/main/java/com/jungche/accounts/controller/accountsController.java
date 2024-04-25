748d8a57d31f63621692cc6d0003ba92f22ef1b2
JungCheChang <jungche0408@gmail.com>
2024-04-25 10:40:29 -0700

    Add Slf4j

--- accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
+++ accounts/src/main/java/com/jungche/accounts/controller/accountsController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Slf4j
 public class accountsController {
 
     private IAccountsService iAccountsService;
