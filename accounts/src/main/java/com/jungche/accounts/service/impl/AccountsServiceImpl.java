6b802b161613581cd1bf2cbd04a065354834f4d5
JungCheChang <jungche0408@gmail.com>
2024-04-24 21:09:36 -0700

    Add auditing

--- accounts/src/main/java/com/jungche/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/jungche/accounts/service/impl/AccountsServiceImpl.java
@@ -36,8 +36,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Leo");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -54,8 +52,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Leo");
         return newAccount;
     }
 
