12011b1e3c60a3136a54774f5a3590f65c4636ba
JungCheChang <jungche0408@gmail.com>
2024-05-22 13:45:28 -0700

    Add API Rate Limiting with Redis

--- accounts/src/main/java/com/jungche/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/jungche/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.jungche.accounts.dto.CustomerDto;
 import com.jungche.accounts.dto.ErrorResponseDto;
 import com.jungche.accounts.dto.ResponseDto;
 import com.jungche.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -230,11 +231,17 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @GetMapping("/contact-info")
